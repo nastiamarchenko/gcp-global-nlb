be8a93a33de4f24de58c90a4112ba45135bffcbc
nastiamarchenko <48097866+nastiamarchenko@users.noreply.github.com>
2019-04-09 09:29:34 +0300

    Create main.tf

---
+++ k8s-app/main.tf
@@ -0,0 +1,87 @@
+variable "external_ip" {}
+variable "node_port" {}
+variable "project_id" {}
+
+resource "kubernetes_service" "default" {
+  metadata {
+    namespace = "default"
+    name      = "morse-app"
+  }
+
+  spec {
+    type             = "NodePort"
+    session_affinity = "ClientIP"
+    external_ips     = ["${var.external_ip}"]
+
+    selector {
+      run = "morse-app"
+    }
+
+    port {
+      name        = "http"
+      protocol    = "TCP"
+      port        = 80
+      target_port = 8000
+      node_port   = "${var.node_port}"
+    }
+  }
+}
+
+resource "kubernetes_config_map" "default" {
+  metadata {
+    name = "morse-app"
+  }
+}
+
+resource "kubernetes_replication_controller" "default" {
+  metadata {
+    name      = "morse-app"
+    namespace = "default"
+
+    labels {
+      run = "morse-app"
+    }
+  }
+
+  spec {
+    selector {
+      run = "morse-app"
+    }
+
+    template {
+      container {
+        image = "gcr.io/${var.project_id}/morse-app:latest"
+        name  = "morse-app"
+
+        resources {
+          limits {
+            cpu    = "0.5"
+            memory = "512Mi"
+          }
+
+          requests {
+            cpu    = "250m"
+            memory = "50Mi"
+          }
+        }
+      }
+
+        
+    }
+  }
+}
+
+resource "kubernetes_horizontal_pod_autoscaler" "default" {
+  metadata {
+    name = "morse-app"
+  }
+  spec {
+    max_replicas = 10
+    min_replicas = 2
+    scale_target_ref {
+      kind = "ReplicationController"
+      name = "morse-app"
+    }
+  }
+}
+
