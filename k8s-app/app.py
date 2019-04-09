be7f7f8a1a3727891cba404a25abb5e8a1e465e8
nastiamarchenko <48097866+nastiamarchenko@users.noreply.github.com>
2019-04-09 09:23:46 +0300

    Update app.py

--- k8s-app/app.py
+++ k8s-app/app.py
@@ -39,16 +39,6 @@ def accept_wrapper(sock):
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
-#    if mask & selectors.EVENT_READ:
-#        recv_data = sock.recv(1024)  # Should be ready to read
-#        if recv_data:
-#             data.outb += recv_data   
-#        else:
-#            print("closing connection to", data.addr)
-#            sel.unregister(sock)
-#            sock.close()
-#    if mask & selectors.EVENT_WRITE:
-#        if data.outb:
     print("sending morse-code to:",  data.addr)
     ipaddr = data.addr[0]
     morsecode = encrypt(ipaddr)
