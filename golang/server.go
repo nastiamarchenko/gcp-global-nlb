5fef095f0f643346ab4cf53f7ceae62c9335c732
nastiamarchenko <48097866+nastiamarchenko@users.noreply.github.com>
2019-04-11 13:27:16 +0300

    Create server.go

---
+++ golang/server.go
@@ -0,0 +1,23 @@
+ln, err := net.Listen("tcp", *addr)
+if err != nil {
+	log.Fatal(err)
+}
+
+for {
+	cn, err := ln.Accept()
+	if err != nil {
+		log.Println("ln.Accept():", err)
+		continue
+	}
+
+	pcn, err := viaproxy.Wrap(cn)
+	if err != nil {
+		log.Println("Wrap():", err)
+		continue
+	}
+
+	log.Printf("remote address is: %v", pcn.RemoteAddr())
+	log.Printf("local address is: %v", pcn.LocalAddr())
+	log.Printf("proxy address is: %v", pcn.ProxyAddr())
+	pcn.Close()
+}
